da8c3c9af2d126af739663b7932d4ba4b5a41a0d
GurjotGrewal123 <gurjot_grewal@outlook.com>
2022-10-14 16:19:20 -0400

    added class Meal

--- Meal.java
+++ Meal.java
@@ -1,3 +1,29 @@
 public class Meal {
 
+    private Pref type1;
+    private Pref type2;
+    private Pref type3;
+    private String name;
+    private double price;
+
+    public Meal(String name, double price, Pref type1) {
+        this.name = name;
+        this.price = price;
+        this.type1 = type1;
+    }
+
+    public Meal(String name, double price, Pref type1, Pref type2) {
+        this.name = name;
+        this.price = price;
+        this.type1 = type1;
+        this.type2 = type2;
+    }
+
+    public Meal(String name, double price, Pref type1, Pref type2, Pref type3) {
+        this.name = name;
+        this.price = price;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.type3 = type3;
+    }
 }
