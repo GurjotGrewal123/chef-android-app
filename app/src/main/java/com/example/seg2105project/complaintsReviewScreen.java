1bb19ecfeaebf91b3b8644baed5ec3698f297e8f
SovasTM <talgerdmc@gmail.com>
2022-11-02 18:35:04 -0400

    Added non-functional UI screens to assist in implementing complaint functionality

---
+++ app/src/main/java/com/example/seg2105project/complaintsReviewScreen.java
@@ -0,0 +1,14 @@
+package com.example.seg2105project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class complaintsReviewScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaints_review_screen);
+    }
+}
