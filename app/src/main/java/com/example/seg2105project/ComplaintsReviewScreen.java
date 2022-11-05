c3d5b741281c1d86a5936df907c2a6eb5104cb44
SovasTM <talgerdmc@gmail.com>
2022-11-04 22:13:59 -0400

    Added complaintPopUp dialog

--- app/src/main/java/com/example/seg2105project/ComplaintsReviewScreen.java
+++ app/src/main/java/com/example/seg2105project/ComplaintsReviewScreen.java
@@ -2,6 +2,7 @@ package com.example.seg2105project;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.ChildEventListener;
@@ -11,9 +12,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,9 +40,6 @@ public class ComplaintsReviewScreen extends AppCompatActivity {
         reference = FirebaseDatabase.getInstance().getReference("complaints");
 
         complaintsList = new ArrayList<>();
-
-        //TO BE IMPLEMENTED
-        addComplaint();
         onItemLongClick();
 
     }
@@ -44,7 +48,7 @@ public class ComplaintsReviewScreen extends AppCompatActivity {
         super.onStart();
         reference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
+            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
                 complaintsList.clear();
                 for(DataSnapshot postSnapshot: dataSnapshot.getChildren()){
                     Complaint complaint = postSnapshot.getValue(Complaint.class);
@@ -60,4 +64,38 @@ public class ComplaintsReviewScreen extends AppCompatActivity {
             }
         });
     }
+
+    private void onItemLongClick(){
+        complaints.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Complaint complaint = complaintsList.get(i);
+                showComplaintDashboardDialog(complaint.getCook().getName(),complaint.getDate().toString());
+                return true;
+            }
+        });
+    }
+
+    private void showComplaintDashboardDialog(final String cookname, final String date){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.activity_complaint_pop_up,null);
+        dialogBuilder.setView(dialogView);
+
+        final TextView cookName = (TextView) dialogView.findViewById(R.id.nameOfCook);
+        final TextView complaintText = (TextView) dialogView.findViewById(R.id.complaintForCook);
+        final TextView dateOfComplaint = (TextView) dialogView.findViewById(R.id.dateComplainedAbout);
+        final TextView adminPrompt = (TextView) dialogView.findViewById(R.id.adminPrompt);
+        final Button permenantlySuspend = (Button) dialogView.findViewById(R.id.permaSuspend);
+        final Button temporarilySuspend = (Button) dialogView.findViewById(R.id.tempSuspend);
+        final Button dismissComplaint = (Button) dialogView.findViewById(R.id.dissmissComplaint);
+        final Button cancelButton = (Button) dialogView.findViewById(R.id.returnButton);
+
+        cookName.setText(cookname);
+        complaintText.setText(date);
+        final AlertDialog b = dialogBuilder.create();
+        b.show();
+
+    }
+
 }
