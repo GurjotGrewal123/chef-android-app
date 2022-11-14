03264e8ad0b180f1d19bd62cd01ce49e8aa15043
SovasTM <talgerdmc@gmail.com>
2022-11-14 18:59:24 -0500

    Added several new screens associated with the Cook class

---
+++ app/src/main/java/com/example/seg2105project/CookModifyMealList.java
@@ -0,0 +1,14 @@
+package com.example.seg2105project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CookModifyMealList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cook_modify_meal_list);
+    }
+}
