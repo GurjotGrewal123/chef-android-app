36925e144e86de41266bf859439b42127cc68c32
SovasTM <talgerdmc@gmail.com>
2022-11-15 20:03:21 -0500

    Added several dialog screens and some non-functional screens

---
+++ app/src/main/java/com/example/seg2105project/AddMealListItem.java
@@ -0,0 +1,14 @@
+package com.example.seg2105project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddMealListItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_meal_list_item);
+    }
+}
