39262ea6882ed13777cff8274c9164c803e2fb6f
Jad Bader <jadbader7@gmail.com>
2022-11-16 18:48:20 -0500

    Added functionality to logout button in cook logged in screen

--- app/src/main/java/com/example/seg2105project/CookLoggedInScreen.java
+++ app/src/main/java/com/example/seg2105project/CookLoggedInScreen.java
@@ -2,13 +2,30 @@ package com.example.seg2105project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CookLoggedInScreen extends AppCompatActivity {
+
+    private Button logOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cook_logged_in_screen);
+
+        logOutButton = findViewById(R.id.cookLogOut);
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logOut();
+            }
+        });
+    }
+    public void logOut(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
