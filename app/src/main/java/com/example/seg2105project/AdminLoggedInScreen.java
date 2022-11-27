c5c693299e99d3411fa5bebbcb8222f3f073b114
GurjotGrewal123 <gurjot_grewal@outlook.com>
2022-11-27 10:36:19 -0500

    fixed admin logged screen navigation

--- app/src/main/java/com/example/seg2105project/AdminLoggedInScreen.java
+++ app/src/main/java/com/example/seg2105project/AdminLoggedInScreen.java
@@ -11,12 +11,14 @@ import android.os.Bundle;
 public class AdminLoggedInScreen extends AppCompatActivity {
 
     Button reviewComplaintsBtn;
+    Button logOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_logged_in_screen);
-        reviewComplaintsBtn = findViewById(R.id.mealsButton);
+        reviewComplaintsBtn = findViewById(R.id.complaintButton);
+        logOutBtn = findViewById(R.id.logOutButton);
 
         reviewComplaintsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,10 +26,22 @@ public class AdminLoggedInScreen extends AppCompatActivity {
                 complaintNav();
             }
         });
+
+        logOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logOutNav();
+            }
+        });
     }
 
     public void complaintNav() {
         Intent intent = new Intent(this, ComplaintsReviewScreen.class);
+        this.startActivity(intent);
+    }
+
+    public void logOutNav(){
+        Intent intent = new Intent(this, MainActivity.class);
         this.startActivity(intent);
     }
 
