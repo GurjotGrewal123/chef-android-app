83df344a98c9fe397ced0726013472dfb3e9840d
SovasTM <talgerdmc@gmail.com>
2022-11-28 19:00:57 -0500

    Added some screens for submitting complaints

---
+++ app/src/main/java/com/example/seg2105project/PreviousOrderItemPreviewScreen.java
@@ -0,0 +1,14 @@
+package com.example.seg2105project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreviousOrderItemPreviewScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_previous_order_item_preview_screen);
+    }
+}
